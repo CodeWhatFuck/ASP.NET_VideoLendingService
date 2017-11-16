eb9d639e1a2cb262c832c1758ad8e7ceeb1b0f1f
bdunams <dabadestboy1@gmail.com>
2017-11-15 21:57:51 -0500

    intial files

---
+++ VideGo/Controllers/MoviesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideGo.Models;
+
+namespace VideGo.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+
+            var movie = new Movie() { Name = "Alien: Covenant" };
+
+            //return View(movie);
+            return Content("Hello World");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id = " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "name";
+
+            return Content(String.Format("pageIndex={0}&sortBy{1}",pageIndex, sortBy));
+        }
+
+        public ActionResult byReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
