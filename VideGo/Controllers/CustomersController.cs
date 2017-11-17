73c20dab035491dee4625177cc5856de5c7cfb5a
bdunams <dabadestboy1@gmail.com>
2017-11-16 19:29:32 -0500

    Added Customer Controller, ViewModel and View

---
+++ VideGo/Controllers/CustomersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideGo.Models;
+using VideGo.ViewModels;
+
+namespace VideGo.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Brian" },
+                new Customer { Name = "Todd" }
+            };
+
+            var customerViewModel = new CustomerViewModel
+            {
+                Customers = customers
+            };
+
+            return View(customerViewModel);
+        }
+
+    }
+}
